5ce2cecc4aba1d9d94977ffd63305101caf3320b
Minsang <minsang1233@gmail.com>
2025-01-12 05:37:36 +0900

    [BE][feat] 회원 프로필 수정 기능 구현

--- src/main/java/com/doubleowner/revibe/domain/user/controller/UserController.java
+++ src/main/java/com/doubleowner/revibe/domain/user/controller/UserController.java
@@ -2,7 +2,9 @@ package com.doubleowner.revibe.domain.user.controller;
 
 import com.doubleowner.revibe.domain.user.dto.request.UserDeleteRequestDto;
 import com.doubleowner.revibe.domain.user.dto.request.UserLoginRequestDto;
+import com.doubleowner.revibe.domain.user.dto.request.UserProfileUpdateRequestDto;
 import com.doubleowner.revibe.domain.user.dto.request.UserSignupRequestDto;
+import com.doubleowner.revibe.domain.user.dto.response.UserProfileResponseDto;
 import com.doubleowner.revibe.domain.user.dto.response.UserSignupResponseDto;
 import com.doubleowner.revibe.domain.user.service.UserService;
 import com.doubleowner.revibe.global.common.dto.CommonResponseBody;
@@ -28,7 +30,6 @@ public class UserController {
 
     /**
      * 회원가입
-     *
      * @param requestDto - 회원가입을 위한 요청 정보
      * @return UserSignupResponseDto - 회원가입 완료 응답 dto
      */
@@ -42,8 +43,7 @@ public class UserController {
 
     /**
      * 로그인
-     *
-     * @param requestDto 로그인 요청 정보
+     * @param requestDto - 로그인 요청 정보
      * @return JWT 토큰 응답
      */
     @PostMapping("/login")
@@ -57,6 +57,12 @@ public class UserController {
 
     /**
      * 회원 삭제
+     * @param requestDto - 회원 삭제 정보 요청
+     * @param userDetails  - 현재 요청 보낸 사용자 식별
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param authentication - 현재 인증된 사용자에 대한 정보
+     * @return - 회원 삭제 응답
      */
     @DeleteMapping()
     public ResponseEntity<CommonResponseBody<Void>> deleteUser(
@@ -75,13 +81,21 @@ public class UserController {
 
         return new ResponseEntity<>(new CommonResponseBody<>("회원삭제가 완료되었습니다."), HttpStatus.OK);
     }
-            /**
-             * 프로필 수정
-             */
 
-            /**
-             * 프로필 조회
-             */
+    /**
+     * 프로필 수정
+     * @param requestDto - 프로필 수정 요청 데이터 dto
+     * @param userDetails - 요청보낸 사용자 정보 제공
+     * @return - 수정된 사용자 프로필 정보 응답
+     */
+    @PutMapping("/profile")
+    public ResponseEntity<CommonResponseBody<UserProfileResponseDto>> updateProfile(
+            @RequestBody UserProfileUpdateRequestDto requestDto,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
+        UserProfileResponseDto responseDto = userService.updateProfile(requestDto, userDetails);
+
+        return new ResponseEntity<>(new CommonResponseBody<>("프로필이 수정되었습니다.", responseDto), HttpStatus.OK);
+    }
 
 }
