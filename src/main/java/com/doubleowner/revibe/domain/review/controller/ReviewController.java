70538fe9e9a47d67efa891947f8b43436a5e4ee6
2unmi <2unmin4030@gmail.com>
2025-01-13 22:22:19 +0900

    [BE][feat] 사용자 별 리뷰 수정 기능 구현

--- src/main/java/com/doubleowner/revibe/domain/review/controller/ReviewController.java
+++ src/main/java/com/doubleowner/revibe/domain/review/controller/ReviewController.java
@@ -2,6 +2,7 @@ package com.doubleowner.revibe.domain.review.controller;
 
 import com.doubleowner.revibe.domain.review.dto.ReviewRequestDto;
 import com.doubleowner.revibe.domain.review.dto.ReviewResponseDto;
+import com.doubleowner.revibe.domain.review.dto.UpdateReviewRequestDto;
 import com.doubleowner.revibe.domain.review.service.ReviewService;
 import com.doubleowner.revibe.global.common.dto.CommonResponseBody;
 import com.doubleowner.revibe.global.config.auth.UserDetailsImpl;
@@ -22,7 +23,7 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @PostMapping(value = "/reviews", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<CommonResponseBody<ReviewResponseDto>> writeReview(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestPart ReviewRequestDto reviewRequestDto, @RequestPart(name = "image") MultipartFile file) {
+    public ResponseEntity<CommonResponseBody<ReviewResponseDto>> writeReview(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestPart ReviewRequestDto reviewRequestDto, @RequestPart(name = "image", required = false) MultipartFile file) {
         ReviewResponseDto review = reviewService.write(reviewRequestDto, file, userDetails.getUser());
         return new ResponseEntity<>(new CommonResponseBody<>("리뷰가 등록되었습니다.", review), HttpStatus.CREATED);
     }
@@ -31,5 +32,12 @@ public class ReviewController {
     public ResponseEntity<CommonResponseBody<List<ReviewResponseDto>>> findReviews(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         List<ReviewResponseDto> read = reviewService.findReview(userDetails.getUser());
         return new ResponseEntity<>(new CommonResponseBody<>("리뷰가 조회 되었습니다", read), HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/reviews/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<CommonResponseBody<Void>> updateReview(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails, @RequestPart UpdateReviewRequestDto updateReviewRequestDto, @RequestPart(name = "image", required = false) MultipartFile file) {
+        reviewService.updateReview(id, userDetails, updateReviewRequestDto, file);
+        return new ResponseEntity<>(new CommonResponseBody<>("리뷰가 수정되었습니다."), HttpStatus.OK);
+
     }
 }
