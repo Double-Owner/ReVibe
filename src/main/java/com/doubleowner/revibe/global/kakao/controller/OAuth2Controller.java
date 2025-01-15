ab5cef6765623dd7bd08a18f9d3bbb7c7ad72ca0
Minsang <minsang1233@gmail.com>
2025-01-15 14:27:28 +0900

    [BE][mod] 로그인 메서드 추가

--- src/main/java/com/doubleowner/revibe/global/kakao/controller/OAuth2Controller.java
+++ src/main/java/com/doubleowner/revibe/global/kakao/controller/OAuth2Controller.java
@@ -1,7 +1,11 @@
 package com.doubleowner.revibe.global.kakao.controller;
 
+import com.doubleowner.revibe.global.common.dto.CommonResponseBody;
+import com.doubleowner.revibe.global.config.dto.JwtAuthResponse;
 import com.doubleowner.revibe.global.kakao.service.OAuth2UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,9 +21,11 @@ public class OAuth2Controller {
      * @param code
      */
     @GetMapping
-    public void generateKakaoToken(@RequestParam String code) {
+    public ResponseEntity<CommonResponseBody<JwtAuthResponse>> generateKakaoToken(@RequestParam String code) {
         String accessToken = oAuth2UserService.generateAccessToken(code);
 
-        oAuth2UserService.getUserInfo(accessToken);
+        JwtAuthResponse userInfo = oAuth2UserService.getUserInfo(accessToken);
+
+        return new ResponseEntity<>(new CommonResponseBody<>("로그인을 성공했습니다.", userInfo), HttpStatus.OK);
     }
 }
