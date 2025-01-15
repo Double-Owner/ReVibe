4220e184e72faf54db9f44fe282d518efccebd75
2unmi <2unmin4030@gmail.com>
2025-01-15 19:23:07 +0900

    [BE][refactor] 리뷰 n+1 문제 수정

--- src/main/java/com/doubleowner/revibe/domain/execution/repository/ExecutionRepository.java
+++ src/main/java/com/doubleowner/revibe/domain/execution/repository/ExecutionRepository.java
@@ -1,9 +1,18 @@
 package com.doubleowner.revibe.domain.execution.repository;
 
 import com.doubleowner.revibe.domain.execution.entity.Execution;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ExecutionRepository extends JpaRepository<Execution, Long> {
+
+    @EntityGraph(attributePaths = {"sell", "payment"})
+    @Query("SELECT p FROM Execution p WHERE p.id = :id")
+    Optional<Execution> findExecutionById(@Param("id") Long id);
 }
--- src/main/java/com/doubleowner/revibe/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/doubleowner/revibe/domain/payment/repository/PaymentRepository.java
@@ -1,7 +1,20 @@
 package com.doubleowner.revibe.domain.payment.repository;
 
 import com.doubleowner.revibe.domain.payment.entity.Payment;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+
+    @EntityGraph(attributePaths = {"buy"})
+    @Query("SELECT p FROM Payment p WHERE p.id = :id")
+    Optional<Payment> findByPaymentId(@Param("id") Long id);
+
 }
