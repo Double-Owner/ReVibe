916aebf7e466c1e23b561ca4bb16b15d1b29d05a
2unmi <2unmin4030@gmail.com>
2025-01-16 17:48:22 +0900

    [BE][refactor] 리뷰 리팩토링

--- src/main/java/com/doubleowner/revibe/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/doubleowner/revibe/domain/review/repository/ReviewRepository.java
@@ -1,6 +1,8 @@
 package com.doubleowner.revibe.domain.review.repository;
 
 import com.doubleowner.revibe.domain.review.entity.Review;
+import com.doubleowner.revibe.global.exception.ReviewException;
+import com.doubleowner.revibe.global.exception.errorCode.ReviewErrorCode;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,6 +16,10 @@ import java.util.Optional;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     Optional<Review> findReviewByIdAndUser_Id(Long id, Long userId);
+
+    default Review findMyReview(Long id, Long userId) {
+        return findReviewByIdAndUser_Id(id, userId).orElseThrow(() -> new ReviewException(ReviewErrorCode.REVIEW_NOT_FOUND));
+    }
 
     @EntityGraph(attributePaths = {"user", "item", "execution"})
     @Query("SELECT r FROM Review r WHERE r.user.id = :userId")
