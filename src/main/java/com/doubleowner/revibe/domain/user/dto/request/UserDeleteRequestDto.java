62dde045ec2de04ba03a119551c4d08b46720ebe
Minsang <minsang1233@gmail.com>
2025-01-18 03:41:41 +0900

    [BE][chore] Dto 어노테이션 추가

--- src/main/java/com/doubleowner/revibe/domain/user/dto/request/UserDeleteRequestDto.java
+++ src/main/java/com/doubleowner/revibe/domain/user/dto/request/UserDeleteRequestDto.java
@@ -1,11 +1,13 @@
 package com.doubleowner.revibe.domain.user.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class UserDeleteRequestDto {
 
+    @NotBlank(message = "비밀번호는 필수 값입니다")
     private final String password;
 
     public UserDeleteRequestDto(@JsonProperty("password") String password) {
--- src/main/java/com/doubleowner/revibe/domain/user/dto/request/UserLoginRequestDto.java
+++ src/main/java/com/doubleowner/revibe/domain/user/dto/request/UserLoginRequestDto.java
@@ -1,5 +1,6 @@
 package com.doubleowner.revibe.domain.user.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,7 +8,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserLoginRequestDto {
 
+    @NotBlank(message = "이메일은 필수 값입니다.")
     private final String email;
+
+    @NotBlank(message = "비밀번호는 필수 값입니다")
     private final String password;
 
 }
