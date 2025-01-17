066a26ede3638f5f83305e4e36a91a717a251a54
dirtbagss <dirtbagag4@gmail.com>
2025-01-17 15:29:38 +0900

    [BE][refactor] 상품 전체조회 N+1 문제해결

--- src/main/java/com/doubleowner/revibe/domain/item/repository/ItemRepository.java
+++ src/main/java/com/doubleowner/revibe/domain/item/repository/ItemRepository.java
@@ -3,15 +3,21 @@ package com.doubleowner.revibe.domain.item.repository;
 import com.doubleowner.revibe.domain.item.entity.Item;
 import com.doubleowner.revibe.global.exception.CommonException;
 import com.doubleowner.revibe.global.exception.errorCode.ErrorCode;
-import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
     default Item findByIdOrElseThrow(Long itemId){
         return findById(itemId).orElseThrow(()->new CommonException(ErrorCode.NOT_FOUND_VALUE,"해당 상품을 찾을 수 없습니다."));
     }
+
+    @Query("SELECT i FROM Item i JOIN FETCH i.brand JOIN FETCH i.user")
+    Page<Item> findAllWithBrandAndUser(Pageable pageable);
 
     boolean existsByName(String name);
 }
