a65b143705682526b1950a7610197d601995fcc0
dirtbagss <dirtbagag4@gmail.com>
2025-01-23 14:15:20 +0900

    [BE][add] redis 설정 구현

---
+++ src/main/java/com/doubleowner/revibe/global/config/RedisConfig.java
@@ -0,0 +1,46 @@
+package com.doubleowner.revibe.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    // RedisTemplate을 String과 Object 타입으로 설정
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        // Key와 Value의 직렬화 방식 설정
+        template.setKeySerializer(new StringRedisSerializer()); // 기본적으로 String 타입
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class)); // JSON 직렬화
+
+        // HashKey와 HashValue의 직렬화 방식 설정
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+
+        return template;
+    }
+}
