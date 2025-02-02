817ff74eda4b24a661bffc05c19c29e9ddc61432
2unmi <2unmin4030@gmail.com>
2025-02-02 19:42:57 +0900

    [BE][add] 유효성 실패시 발생하는 예외 핸들러 추가 + todo 하드코딩된 "Failed Validation" 수정

--- src/main/java/com/doubleowner/revibe/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/doubleowner/revibe/global/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import com.doubleowner.revibe.global.common.dto.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -20,6 +21,18 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
                 .body(new CommonResponseBody<>(e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> handleValidException(MethodArgumentNotValidException e) {
+
+        ErrorResponseDto message = ErrorResponseDto.builder()
+                .error(e.getStatusCode().toString())
+                .code("Failed Validation") // todo 하드코딩된 code 수정
+                .message(e.getBindingResult().getFieldError().getDefaultMessage())
+                .build();
+        return new ResponseEntity<>(message, e.getStatusCode());
+
     }
 
 }
