ac13b9985c0e42e64d2f9d53371fd1dfabe7b224
2unmi <2unmin4030@gmail.com>
2025-02-05 16:44:13 +0900

    [BE][mod] 권한 경로 설정 2차

--- src/main/java/com/doubleowner/revibe/global/config/WebConfig.java
+++ src/main/java/com/doubleowner/revibe/global/config/WebConfig.java
@@ -1,13 +1,12 @@
 package com.doubleowner.revibe.global.config;
 
-import com.doubleowner.revibe.domain.user.entity.Role;
 import com.doubleowner.revibe.global.config.filter.JwtAuthFilter;
 import jakarta.servlet.DispatcherType;
-import jodd.net.HttpMethod;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,6 +16,8 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static com.doubleowner.revibe.domain.user.entity.Role.ROLE_ADMIN;
 
 @Configuration
 @EnableWebSecurity
@@ -28,10 +29,7 @@ public class WebConfig {
     private final AuthenticationEntryPoint authenticationEntryPoint;
     private final AccessDeniedHandler accessDeniedHandler;
 
-    private static final String[] WHITE_LIST = { "/api/users/**", "/oauth2/**", "/login/oauth2/code/**", "/oauth/authorize"};
-    private static final String[] AUTHENTICATED_URLS = {"/api/reviews/**","/api/executions", "/api/items/{itemId}/wishlists", "/api/wishlists", "/chats/**", "/chat/sendMessage/{roomId}", "/chatrooms/{roomId}/messages"};
-    private static final String[] USER_ONLY_URLS = {"/api/accounts/**", "/api/buy-bids/**", "/api/sell-bids/**", "/api/carts/**",
-            "/api/items/{itemId}/wishlists","/api/wishlists", "/api/v1/payments", "/v1/payments/confirm", "/confirm/payment", "payments", "/api/issued-coupons/**"};
+    private static final String[] WHITE_LIST = {"/api/users/**", "/oauth2/**", "/login/oauth2/code/**", "/oauth/authorize"};
     private static final String[] ADMIN_ONLY_URLS = {"/api/brands", "/api/coupons/**"};
 
     @Bean
@@ -40,15 +38,12 @@ public class WebConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(WHITE_LIST).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/items/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/items/**").hasRole(ROLE_ADMIN.getName())
+                        .requestMatchers(HttpMethod.PATCH, "/api/items/**").hasRole(ROLE_ADMIN.getName())
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                         .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.INCLUDE, DispatcherType.ERROR).permitAll()
-                        // path 별로 접근이 가능한 권한 설정
-                        // 로그인한 사용자만 접근 가능 (USER 또는 ADMIN)
-                        .requestMatchers(AUTHENTICATED_URLS).authenticated()
-                        // USER 궎한이 필요한 API
-                        .requestMatchers(USER_ONLY_URLS).hasRole(Role.ROLE_USER.getName())
-                        // ADMIN 권한이 필요한 API
-                        .requestMatchers(ADMIN_ONLY_URLS).hasRole(Role.ROLE_ADMIN.getName())
+                        .requestMatchers(ADMIN_ONLY_URLS).hasRole(ROLE_ADMIN.getName())
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(handler -> handler
